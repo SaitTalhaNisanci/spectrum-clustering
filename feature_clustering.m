58e7761b6961240c02fdfc696452081f94bafcaa
alperenyakut <alperenyakut@gmail.com>
2017-12-18 19:37:49 +0300

    Add charts

--- feature_clustering.m
+++ feature_clustering.m
@@ -1,8 +1,7 @@
 load('realdata.mat');
-
 K = 10;
-A = X';
-
+A = [mean(X,1)' var(X,0,1)'];
+A = A(1:10,:);
 norm_clusters = normalized_spec(A, K);
 unnorm_clusters = unnormalized_spec(A, K);
 kmeans_clusters = kmeans(A, K);
